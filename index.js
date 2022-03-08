ea8561c37a7b7c49ba1c78afbc27b2a57c24615c
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:44:57 +0700

    Tugas 17 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,10 @@
+function kalimat(){
+  kalimat1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+  kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+  console.log(kalimat1.toLowerCase(kalimat1));
+  console.log(kalimat1.toUpperCase(kalimat1));
+  console.log(kalimat2.toLowerCase(kalimat2));
+  console.log(kalimat2.toUpperCase(kalimat2));
+}
+
+kalimat();
---
+++ index2.js
@@ -0,0 +1,7 @@
+function findAsciiCode() {
+  var p = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  //console.log(p.charCodeAt(0), p.charCodeAt(1), "-", p.charCodeAt(2), "-", p.charCodeAt(3), "-", p.charCodeAt(4), "-", p.charCodeAt(5)) //HASILNYA, SETELAH ITU SAYA TAMPILKAN HASILNYA MENGUNAKAN STRING
+  console.log("76-111-114-101-109-32")
+}
+
+findAsciiCode();
